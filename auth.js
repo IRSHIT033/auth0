16fdd8112a6ddb234133aecc602a481c2a635c63
IRSHIT033 <mukherjeeirshit50@gmail.com>
2024-10-18 10:52:02 +0530

    aas

---
+++ auth.js
@@ -0,0 +1,37 @@
+const { ManagementClient } = require("auth0");
+
+const management = new ManagementClient({
+  domain: process.env.MANAGEMENT_DOMAIN,
+  clientId: process.env.MANAGEMENT_CLIENT_ID,
+  clientSecret: process.env.MANAGEMENT_CLIENT_SECRET,
+  scope: "create:organization_invitations",
+});
+
+// Organization ID from Auth0
+const organizationId = "org_aXtq6tMfnNLld8Bi"; // Replace with your org ID
+
+// Function to send an invitation
+async function sendOrgInvitation(userEmail) {
+  try {
+    const invitation = await management.organizations.createInvitation(
+      {
+        id: organizationId,
+      },
+      {
+        invitee: {
+          email: userEmail,
+        },
+        inviter: {
+          name: "Your App Name",
+        },
+        client_id: process.env.CLIENT_ID,
+        connection_id: process.env.CONNECTION_ID, // Connection used for signup/login
+      }
+    );
+    console.log("Invitation sent: ", invitation);
+  } catch (error) {
+    console.error("Error sending invitation: ", error);
+  }
+}
+
+module.exports = { sendOrgInvitation };
---
+++ index.js
@@ -0,0 +1,37 @@
+const { auth } = require("express-openid-connect");
+const express = require("express");
+
+const app = express();
+const { sendOrgInvitation } = require("./auth");
+require("dotenv").config();
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get("/", (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+});
+
+app.get("/invite", async (req, res) => {
+  try {
+    await sendOrgInvitation("mukherjeeirshit50@gmail.com");
+    res.send("Invitation sent");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(`Error sending invitation, ${error}`);
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
