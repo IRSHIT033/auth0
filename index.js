640ec727a08f0d877a03cf77f3b4eaa7aac75b8b
IRSHIT033 <mukherjeeirshit50@gmail.com>
2024-10-18 11:32:37 +0530

    added invite

--- index.js
+++ index.js
@@ -17,6 +17,15 @@ const config = {
 // auth router attaches /login, /logout, and /callback routes to the baseURL
 app.use(auth(config));
 
+app.get("/login", (req, res) => {
+  res.oidc.login({
+    authorizationParams: {
+      organization: req.query.organization,
+      invitation: req.query.invitation,
+    },
+  });
+});
+
 // req.isAuthenticated is provided from the auth router
 app.get("/", (req, res) => {
   res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
